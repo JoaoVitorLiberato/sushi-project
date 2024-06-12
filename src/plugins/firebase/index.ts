7ba8248313987860e5fcb1458ba6a5835f8ee32b
João Vitor Liberato <j.liberato@vertex.vertexdigital.co>
2024-06-12 18:50:11 -0300

    Adicionando firebase e criando eventos

---
+++ src/plugins/firebase/index.ts
@@ -0,0 +1,21 @@
+import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app"
+import { getAnalytics, logEvent } from "firebase/analytics"
+
+const FIREBASE_CONFIG:FirebaseOptions = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_APP_ID
+}
+
+const FIREBASE_APP = initializeApp(FIREBASE_CONFIG)
+const ANALYTICS = getAnalytics(FIREBASE_APP as FirebaseApp)
+
+logEvent(ANALYTICS, "app_start")
+
+export function event (event:string, params?: Record<string, string|string[]|number|boolean>):void {
+  logEvent(ANALYTICS, event, params || {})
+}
